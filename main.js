3cdf51b0b57f0b8717b330c0c935272dc45d63bd
Sergey <sergey3>
2023-03-06 22:30:19 +0500

    v1

---
+++ main.js
@@ -0,0 +1,53 @@
+let time = +prompt('Введите время')
+
+switch(time) {
+    case 1: alert('1 час ночи');
+    break;
+    case 2: alert('2 часа ночи');
+    break;
+    case 3: alert('3 часа ночи');
+    break;
+    case 4: alert('4 часа ночи');
+    break;
+    case 5: alert('5 часов ночи');
+    break;
+    case 6: alert('6 часов ночи');
+    break;
+    case 7: alert('7 часов утра');
+    break;
+    case 8: alert('8 часов утра');
+    break;
+    case 9: alert('9 часов утра');
+    break;
+    case 10: alert('10 часов утра');
+    break;
+    case 11: alert('11 часов утра');
+    break;
+    case 12: alert('12 часов утра');
+    break;
+    case 13: alert('13 часов дня');
+    break;
+    case 14: alert('14 часов дня');
+    break;
+    case 15: alert('15 часов дня');
+    break;
+    case 16: alert('16 часов дня');
+    break;
+    case 17: alert('17 часов дня');
+    break;
+    case 18: alert('18 часов дня');
+    break;
+    case 19: alert('19 часов вечера');
+    break;
+    case 20: alert('20 часов вечера');
+    break;
+    case 21: alert('21 час вечера');
+    break;
+    case 22: alert('22 часа вечера');
+    break;
+    case 23: alert('23 часа вечера');
+    break;
+    case 24: alert('24 часa вечера');
+    break;
+    default: alert("Время не верно")
+}
